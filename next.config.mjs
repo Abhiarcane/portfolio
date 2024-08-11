7c19c2e3b99b21f3f7d91c1adc162883f2859785
Your Name <you@example.com>
2024-08-11 19:49:30 +0530

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withNextOnNetlify from 'next-on-netlify';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Your existing Next.js configuration options
+};
+
+export default withNextOnNetlify(nextConfig);
